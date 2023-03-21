b64561e854983df8651a1c1bb57a5edafa40298f
Sk. Nahid <118644301+nahid2700@users.noreply.github.com>
2023-03-21 11:39:13 +0600

    Add files via upload

---
+++ doc_string.py
@@ -0,0 +1,5 @@
+def comment(n):
+    '''Hi this is not just a comment.'''
+    print(n**2)
+comment(5)
+print(comment.__doc__)    # using doc string we can print out the hidden text from function for documentation the code
