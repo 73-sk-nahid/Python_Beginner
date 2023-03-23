3f4fb582f0f131944c2932809fe50bf9a823b06b
Sk. Nahid <118644301+nahid2700@users.noreply.github.com>
2023-03-23 23:48:57 +0600

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,9 @@
+# sets are unordered collection of data.
+# it does not contain duplicate items.
+# sets aer unchangeable
+# does not maintain any order
+
+set = {"nahid", 73, 23, 3.43}
+print(type(set))  # for confirming the type of this. Is it actually set or not.
+print(set)   # in my IDE the output is <class 'set'> {73, 3.43, 'nahid', 23}
+# it can be different in your IDE
